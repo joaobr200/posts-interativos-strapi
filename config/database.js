55cdd0f126d06bf49f78d843adf3e0d649279f03
Joaobr200 <joaoarantesjob@gmail.com>
2020-10-01 00:50:52 -0300

    config database

--- config/database.js
+++ config/database.js
@@ -1,15 +1,40 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+module.exports = ({ env }) => {
+  if (env("NODE_END") === "development") {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "sqlite",
+            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    };
+  } else {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: env("DATABASE_HOST", "localhost"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "strapi"),
+            username: env("DATABASE_USERNAME", "strapi"),
+            password: env("DATABASE_PASSWORD", "strapi"),
+            schema: "public",
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-    },
-  },
-});
+    };
+  }
+};
